ba6331a7fff6f9742d67faab7dc2629851e393cc
Ken Nguyen <ikendoit1998@gmail.com>
2018-08-19 16:16:12 -0700

    set up spawn process

--- queries/Issues.js
+++ queries/Issues.js
@@ -1,31 +1,29 @@
 const fetch = require('node-fetch')
+const { execSync } = require('child_process');
 
 const repoIssue = async ({repo, user, cred, state, owner}) => {
+	
+	if (!repo || !owner) {
 
-	let result, response;
+		// auto fetch current repo info
+		const spawnRes = execSync(`echo -e "${user}" -e "${cred}" | git remote show origin`, [], {stdio:[0,1,2]});
+		const repoUrl = /https(.*)/.exec(spawnRes.toString())
+		const githubParams = /github.com\/(\w+)\/(\w+)/.exec(repoUrl)
+		owner = githubParams[1]
+		repo = githubParams[2]
 
-	if (repo && owner) {
-
-		result = await fetch(`https://api.github.com/repos/${owner}/${repo}/issues?state=${state}`, {
-			headers: {
-				Authorization: `token ${cred}`
-			},
-			method: 'GET', 
-		})
-
-		response = await result.json()
-
-	} else {
-
-		result = await fetch(`https://api.github.com/user/issues?state=${state}`, {
-			headers: {
-				Authorization: `token ${cred}`
-			},
-			method: 'GET', 
-		})
-
-		response = await result.json()
 	}
+
+	if (!repo || !owner) throw new Error('No Permission to checki repo information')
+
+	const result = await fetch(`https://api.github.com/repos/${owner}/${repo}/issues?state=${state}`, {
+		headers: {
+			Authorization: `token ${cred}`
+		},
+		method: 'GET', 
+	})
+
+	response = await result.json()
 
   if (response.length === 0 || !response[0] || !response[0].url) {
     console.log('no issues')
