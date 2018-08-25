b77df47026d01258aef575b87308d784e2331d8f
Ken Nguyen <ikendoit1998@gmail.com>
2018-08-24 18:00:36 -0700

    draw boxes, need work on line calculation

--- index.js
+++ index.js
@@ -1,63 +1,126 @@
 #!/usr/bin/env node
-const program         = require('commander');
-const issueQueries    = require('./queries/Issues')
-const prQueries       = require('./queries/PullRequests')
 
-const user            = process.env.USER_GITHUB
-const cred            = process.env.CRED_GITHUB
-
-if (!user || !cred) return "please export environment variables"
-
-program
-.command('<resource>', 'the resource to choose: "prs"|"issues"')
-
-.description('fetch Issues and PRs')
-
-.option('-s, --state <state>', 'the state to check: "open"|"closed"|"all"|"ALL"')
-.option('-r, --repo <repo>', 'the repository to check')
-.option('-o, --owner <owner>', 'the owner of the repository to check')
-.action( async () => {
-
-	let state, repo, owner;
-
-	try { 
-		switch (program.args[0].toUpperCase()) {
-
-			case 'PRS':
-				state = program.state || 'open'
-				repo 	= program.repo || null
-				owner = program.owner || null
-				if (!repo || !owner) return "please specify owner + repo"
-				await prQueries.repoPullRequest({ 
-					repo, 
-					user, 
-					cred, 
-					owner,
-					state,
-				});
-				break;
-			case 'ISSUES': 
-
-				state = program.state || 'open'
-				repo 	= program.repo || null
-				owner = program.owner || null
-				await issueQueries.repoIssue({
-					repo, 
-					user, 
-					cred, 
-					state,
-					owner,
-				})
-				break;
-
-			default:
-				console.log('invalid command');
-		}
-  } catch(err) {
-		console.log('woops, error, please contact me or check the log')
-		console.log(err.message)
+const print             = (chr) =>{
+	process.stdout.write(`${chr}`)
+}
+const drawLine          = (width,chr) => {
+	for (let i = 0 ; i < width ; i++){
+		print(chr)
 	}
+}
+
+// draw boxes by line
+const drawBoxesByLine = ({ dataArray, ROW, COLUMN, NUM_BOX, PADDING_LEFT }) => {
+
+  // draw padding -> draw line ceil
+  for (let i = 0 ; i < NUM_BOX ; i++){
+    drawLine(PADDING_LEFT,' ')
+    drawLine(ROW,'-');	
+  }
+
+  print('\n')
+
+  // draw '|' when i===padding-1
+  for (let row = 0 ; row < ROW ; row++){
+
+    const lineWidth  = COLUMN*(NUM_BOX-1)
+
+    for (let i = 0 ; i < lineWidth + PADDING_LEFT*NUM_BOX ; i++){
+
+      const boxNum     =   Math.floor( i / ( ROW+PADDING_LEFT ) )
+      const atFirst    =   i === PADDING_LEFT-1
+      const atRowStart = ( i % (ROW+PADDING_LEFT) === 0 )
+      let   dataDraw   =   dataArray[boxNum] || ''
+      const columnPadd = ( ( i-PADDING_LEFT ) % COLUMN === 0 ) ||
+                         ( ( i+PADDING_LEFT ) % COLUMN === 0 ) 
+
+      if ( atRowStart ||  atFirst ) {
+        print('|')
+        print(' ')
+        i++
+      } else {
+        if ( dataDraw[0] && (i >= PADDING_LEFT || columnPadd) ) {
+          print(dataDraw[0])
+          dataArray[boxNum] = dataArray[boxNum].slice(1)
+        } else {
+          print(' ')
+        }
+      }
+    }
+
+    print('\n')
+
+  }
+
+  for (let i = 0 ; i < NUM_BOX ; i++){
+    drawLine(PADDING_LEFT,' ')
+    drawLine(ROW,'-');	
+  }
+
+  print('\n')
+
+}
+
+// allocate boxes size, width, num box/line based on dataArray [ "...", "..." ]
+const drawBoxes = ({ dataArray, PADDING_LEFT }) => {
+  let windowWidth = process.env.COLUMN
+  let ROW = -1;
+  let COLUNN = -1;
+  let NUM_BOX = 4;
+  dataArray.forEach( str => {
+    str.length()
+  });
+}
+
+// draw 4 square boxes each row, 27x20
+const config =  {
+  ROW            : 20
+  COLUMN         : 27
+  NUM_BOX        : 4
+  PADDING_LEFT   : 1
+}
+
+const data              = [
+  {
+    name: 'name 1aha1123456789123asfsadfdf'
+  },
+  {
+    name: 'name 2'
+  },
+  {
+    name: 'name 3'
+  },
+  {
+    name: 'name 4'
+  },
+  {
+    name: 'name 3 again'
+  },
+  {
+    name: 'name fourth'
+  },
+  {
+    name: 'name fith'
+  },
+  {
+    name: 'name filthyyyy'
+  },
+  {
+    name: 'name checking '
+  },
+  {
+    name: 'name maybe last one ?'
+  },
+  {
+    name: 'name what \n is this'
+  },
+  {
+    name: 'name can I trust this?'
+  },
+]
 
 
+drawBoxesByLine({
+  dataArray: data.map(ele => ele.name),
+  ...config,
 })
-.parse(process.argv);
